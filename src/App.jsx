b5804228f59cd205d3de19e51d93c4de9f767df1
severbit <ernurdenasan@gmail.com>
2026-05-24 07:40:43 +0500

    added: features component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero/>
       <About/>
+      <Features/>
     </main>
   );
 }
